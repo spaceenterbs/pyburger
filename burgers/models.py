4a54bda78aa63b65ca59240f8d446bf502ecfade
enterspacebs <anotherhell48@gmail.com>
2023-08-16 23:00:35 +0900

    name it

--- burgers/models.py
+++ burgers/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
+class Burger(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField(default=0)
+    calories = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
 """
 class 햄버거:
     이름
