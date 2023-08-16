17a3d503c50fa77abed24e10a704b832109918bd
enterspacebs <anotherhell48@gmail.com>
2023-08-16 22:43:54 +0900

    models initialized

---
+++ burgers/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+"""
+class 햄버거:
+    이름
+    가격
+    칼로리
+    
+models.Model 클래스를 상속받아야 한다.
+Django가 제공하는 models.Model 클래스는 정의한 클래스가 데이터베이스에서 하나의 테이블 역할을 할 수 있도록 도와준다.
+"""
--- config/views.py
+++ config/views.py
@@ -1,11 +1,30 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+# from django.http import HttpResponse
+
+
 def main(request):
-    return render(request, "main.html") # HttpResponse 대신 render 함수 사용
-    # HttpResponse("안녕하세요, pyburger입니다")
-    # View 함수가 문자열을 리턴하고 싶다면, 언제나 HttpResponse 객체 안에 담아서 돌려준다!
+    return render(request, "main.html")
+
 
 def burger_list(request):
-    return render(request, "burger_list.html") # HttpResponse 대신 render 함수 사용
-    # HttpResponse("pyburger의 햄버거 목록입니다")
+    return render(request, "burger_list.html")
+
+
+# def main(request):
+#     return HttpResponse("안녕하세요, pyburger입니다")
+
+
+# def burger_list(requset):
+#     return HttpResponse("pyburger의 햄버거 목록입니다")
+
+
+# def main(request):
+#     return render(request, "main.html")  # HttpResponse 대신 render 함수 사용
+#     # HttpResponse("안녕하세요, pyburger입니다")
+#     # View 함수가 문자열을 리턴하고 싶다면, 언제나 HttpResponse 객체 안에 담아서 돌려준다!
+
+
+# def burger_list(request):
+#     return render(request, "burger_list.html")  # HttpResponse 대신 render 함수 사용
+#     # HttpResponse("pyburger의 햄버거 목록입니다")
