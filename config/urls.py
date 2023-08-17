5e975469ed71eee7dcec5861a1e43850f761b209
enterspacebs <anotherhell48@gmail.com>
2023-08-17 23:37:01 +0900

    GET added

--- config/urls.py
+++ config/urls.py
@@ -16,15 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# burger_list 함수를 추가적으로 import 한다
-from config.views import main, burger_list # views.py에 작성한 main 함수를 가져온다.
 
-urlpatterns = [ # 각 path 항목은 메뉴를 나타낸다.
-    path('admin/', admin.site.urls),
+# burger_list 함수를 추가적으로 import 한다
+from config.views import (
+    main,
+    burger_list,
+    burger_search,
+)  # views.py에 작성한 main 함수를 가져온다.
+
+urlpatterns = [  # 각 path 항목은 메뉴를 나타낸다.
+    path("admin/", admin.site.urls),
     # 경로를 지정하지 않으면 main 직원을 호출한다
-    path("", main), # 공백(아무것도 입력하지 않은 경로)과 main 함수를 연결
+    path("", main),  # 공백(아무것도 입력하지 않은 경로)과 main 함수를 연결
     # 'burgers' 경로로 접근하면 burger_list라는 직원을 호출한다
     # View 함수(직원)의 이름은 burger_list인데, URLconf에서의 이름(메뉴명)은 burgers/이다.
     # 둘의 이름이 달라도 된다.
     path("burgers/", burger_list),
+    path("search/", burger_search),
 ]
--- config/views.py
+++ config/views.py
@@ -10,14 +10,26 @@ def burger_list(request):
     # return render(request, "burger_list.html")
 
     # Template로 전달해줄 dict 객체
-    context = {
-        "burgers": burgers,  # burgers 키에 burgers 변수(QuerySet)를 전달한다.
-    }
+    context = {"burgers": burgers}  # burgers 키에 burgers 변수(QuerySet)를 전달한다.
     return render(request, "burger_list.html", context)
 
 
 def main(request):
     return render(request, "main.html")
+
+
+def burger_search(request):
+    # request.GET에서 "keyword"라는 키로 전달된 데이터를 가져와 출력
+    keyword = request.GET.get("keyword")
+    print(keyword)
+    # 1 print(request.GET)  # request.GET으로 전달된 데이터를 출력
+
+    # 이름(name 속성)에 전달받은 키워드 값이 포함된 Burger를 검색
+    burgers = Burger.objects.filter(name__contains=keyword)
+    # 2 print(burgers)
+    context = {"burgers": burgers}
+
+    return render(request, "burger_search.html", context)
 
 
 # def burger_list(request):
