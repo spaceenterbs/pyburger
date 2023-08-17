11925c785353bc48f31c39ce56c06261d5d9e177
enterspacebs <anotherhell48@gmail.com>
2023-08-17 21:39:59 +0900

    views let's go

--- config/views.py
+++ config/views.py
@@ -17,14 +17,3 @@ def burger_list(request):
 
 # def burger_list(requset):
 #     return HttpResponse("pyburger의 햄버거 목록입니다")
-
-
-# def main(request):
-#     return render(request, "main.html")  # HttpResponse 대신 render 함수 사용
-#     # HttpResponse("안녕하세요, pyburger입니다")
-#     # View 함수가 문자열을 리턴하고 싶다면, 언제나 HttpResponse 객체 안에 담아서 돌려준다!
-
-
-# def burger_list(request):
-#     return render(request, "burger_list.html")  # HttpResponse 대신 render 함수 사용
-#     # HttpResponse("pyburger의 햄버거 목록입니다")
